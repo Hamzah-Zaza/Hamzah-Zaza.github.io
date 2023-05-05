111a887da48ae37709682581fb03e749933812a6
Hamzah Zaza <128675840+Hamzah-Zaza@users.noreply.github.com>
2023-05-04 23:40:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// Add an event listener to the search form
-const container = document.querySelector('#data-container');
-
-// Fetch data from the API
-fetch('https://github.com/Hamzah-Zaza')
-  .then(response => response.json())
-  .then(data => {
-    // Create an element to display the data
-    const item = document.createElement('div');
-    item.innerHTML = `
-      <h2>${data.title}</h2>
-      <p>${data.completed ? 'Completed' : 'Not Completed'}</p>
-    `;
-    // Append the element to the container
-    container.appendChild(item);
+const descriptionText = document.querySelector('.description-text');
+fetch('https://raw.githubusercontent.com/Hamzah-Zaza/test-repository/main/description.txt')
+  .then(response => response.text())
+  .then(text => {
+    descriptionText.textContent = text;
   })
   .catch(error => console.error(error));
